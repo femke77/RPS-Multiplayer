4cf0c6ef86f333bf257505cb6d16c4678f7eb2b5
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-17 14:28:15 -0800

    set up firebase database and basic grid layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAaI2m9pi4vHSG9qX4-_2VtgP7M3tRoy68",
+    authDomain: "rps-multiplayer-e819c.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-e819c.firebaseio.com",
+    projectId: "rps-multiplayer-e819c",
+    storageBucket: "rps-multiplayer-e819c.appspot.com",
+    messagingSenderId: "461944139523",
+    appId: "1:461944139523:web:fd0636561bcbeb24d34336"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+  
+  var database = firebase.database();
+
+  
