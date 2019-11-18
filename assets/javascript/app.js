f13a73a664a71ea809aa3329313b0613ac7299b2
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-17 20:07:42 -0800

    modals for player names working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,70 @@ var firebaseConfig = {
     storageBucket: "rps-multiplayer-e819c.appspot.com",
     messagingSenderId: "461944139523",
     appId: "1:461944139523:web:fd0636561bcbeb24d34336"
-  };
- 
-  firebase.initializeApp(firebaseConfig);
-  
-  var database = firebase.database();
+};
 
-  
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+$(document).ready(function () {
+
+    var playerOne = {
+        name: "john",
+        wins: 0,
+        losses: 0
+    }
+
+    var playerTwo = {
+        name: "sally",
+        wins: 0,
+        losses: 0
+    }
+
+
+    if (playerOne.name == -1) {
+        $(".start").append(
+            $("<button/>", {
+                text: "Start Game",
+                id: "start-btn",
+                "data-toggle": "modal",
+                "data-target": "#name-modal",
+            }
+            ));
+
+    } else if (playerOne.name != -1 && playerTwo.name == -1) {
+        $(".start").append(
+            $("<button/>", {
+                text: "Join Game",
+                id: "start-btn",
+                "data-toggle": "modal",
+                "data-target": "#name-modal"
+            }
+            ));
+        
+    }
+
+    $("#save").on("click", function () {
+        if (playerOne.name == -1){
+            playerOne.name = $("#player-name").val();
+            $(".player").append("1: " + playerOne.name)
+        } else {
+            playerTwo.name = $("#player-name").val();
+            $(".player").append("2: " + playerTwo.name)
+        }
+
+
+       
+    })
+
+
+
+
+
+
+
+
+
+
+
+});  //end doc ready
