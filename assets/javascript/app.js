f79d6b52e90c2df3ca5a3451685d411e9ebd9b68
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-28 00:04:34 -0800

    made game ref, pushing each user's guess to db, game ctrl code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,68 +14,165 @@ var database = firebase.database();
 
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
+var gameRef = database.ref("game");
+
 var numChildren = 0;
+var guess;
+var key;
 
 var playerOne = {
     name: "",
+    id: null,
     wins: 0,
     losses: 0
 }
 
 var playerTwo = {
     name: "",
+    id: null,
     wins: 0,
     losses: 0
 }
 
+//start game with no access to buttons until two players ready
+// disableRPS();
 
+//  .info/connected (t/f)
 connectedRef.on("value", function (snap) {
     if (snap.val()) {
-        var con = connectionsRef.push(true);
-        con.onDisconnect().remove();
+        var con = connectionsRef.push(true);      
+        key = con.getKey(); 
+        con.onDisconnect().remove().then(function(){
+              //removes the game after all players disconnect
+              gameRef.remove();
+        });
+        
     }
+
 });
 
+
+//  /connections
 connectionsRef.on("value", function (snap) {
     numChildren = snap.numChildren();
     $(".chat").text(numChildren);
-    
-    if (numChildren === 1) {
-       
-    
-    } else if (numChildren === 2 ) {
-       
-    
-    } else {
-        $(".chat").text("2 are playing");
+
+    if (numChildren == 2){
+        //this is where we will enable the rps buttons for play
+        $(".game-output").html("Ready to play! Make your choice. <br/>")
+    } else if (numChildren == 1){
+        $(".game-output").html("Waiting for opponent... <br/>")
     }
-    
 
-})
-
-//-------------------------------------------------------
-
-
-
-
-
-    
-$("#save-btn").on("click", function (event) {
-    event.preventDefault();
-    if (numChildren == 1) {
-        playerOne.name = $("#player-name").val();
-        $(".player").append(": " + playerOne.name)
-    } else {
-        playerTwo.name = $("#player-name").val();
-        $(".player").append(": " + playerTwo.name)
-
-    }
 
 });
 
 
 
+ 
+$("#save-btn").on("click", function (event) {
+    event.preventDefault();
+    //this player 1 player two stuff is not needed and must be removed to just "player"
+    
+    var name = $("#player-name").val().trim();
+    if (name.length > 0) {
+        if (numChildren == 1) {
+            playerOne.name = name;
+           
+            $(".player").append(": " + playerOne.name)
+            gameRef.child(key).set({
+                
+                name: playerOne.name
+            })
+            $("#save-btn").prop("disabled", true);
+        } else if (numChildren == 2) {
+            playerTwo.name = name;
+            gameRef.child(key).set({
+                
+                name: playerTwo.name
+            })
+            $(".player").append(": " + playerTwo.name)
+            $("#save-btn").prop("disabled", true);
 
+
+        } else {
+            $(".game-output").text("Two players are currently playing. Try again later.");
+        }
+        
+    } else {
+        $(".game-output").text("Enter a name of at least 1 character, please.")
+    }
+
+    $("form").trigger("reset");
+    
+  
+    
+    
+});
+
+function disableRPS(){
+    $("#rock-btn").prop("disabled", true);
+    $("#paper-btn").prop("disabled", true);
+    $("#scissors-btn").prop("disabled", true);
+}
+
+function enableRPS(){
+    $("#rock-btn").prop("disabled", false);
+    $("#paper-btn").prop("disabled", false);
+    $("#scissors-btn").prop("disabled", false);
+}
+
+
+
+
+$("#rock-btn").on("click", function () {
+    $(".game-output").append("you chose rock")
+    guess = "r";
+    gameRef.child(key).update({
+        
+        guess: guess
+       
+    });
+  
+//    disableRPS()
+});
+
+$("#paper-btn").on("click", function () {
+    guess = "p"
+    $(".game-output").append("you chose paper <br/>");
+    gameRef.child(key).update({
+        
+        guess: guess
+       
+    });
+//    disableRPS()
+});
+
+$("#scissors-btn").on("click", function () {
+    guess = "c";
+    $(".game-output").append("you chose scissors <br/>")
+    gameRef.child(key).update({
+        
+        guess: guess
+       
+    });
+//    disableRPS()
+});
+
+//tie this logic to the rps buttons, score the game and reenable buttons
+if ((guess === "r") || (guess === "p") || (guess === "s")) {
+
+    if ((guess === "r" && computerGuess === "s") ||
+        (guess === "s" && computerGuess === "p") ||
+        (guess === "p" && computerGuess === "r")) {
+        wins++;
+    } else if (guess === computerGuess) {
+        ties++;
+    } else {
+        losses++;
+    }
+
+}
 
 
 
