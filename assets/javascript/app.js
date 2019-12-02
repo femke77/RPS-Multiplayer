8d02cbb33df5ea07a34461eb14cd14803852ca65
Meg Meyers <megan.g.meyers@gmail.com>
2019-12-01 22:37:54 -0800

    working game logic, working chat functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,12 @@
-var firebaseConfig = {
+/** TODO:
+ *     Control play with button disabling
+ *     Add names to chat functionality
+ * ----------------------------------------------------
+ *     
+ *     Make a nicer element for player scores
+ */
+
+ var firebaseConfig = {
     apiKey: "AIzaSyAaI2m9pi4vHSG9qX4-_2VtgP7M3tRoy68",
     authDomain: "rps-multiplayer-e819c.firebaseapp.com",
     databaseURL: "https://rps-multiplayer-e819c.firebaseio.com",
@@ -11,34 +19,40 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
-
-var connectionsRef = database.ref("/connections");
+var connectionsRef = database.ref("/connections/");
 var connectedRef = database.ref(".info/connected");
-var gameRef = database.ref("/game");
 
 var numChildren = 0;
-var guess;
-var key;
 
 var player = {
+    key: "",
     name: "",
+    guess: "",
     wins: 0,
-    losses: 0
+    losses: 0,
+    ties: 0
 }
 
+var opponent = {
+    key: "",
+    name: "",
+    guess: ""
+}
 
 
 //start game with no access to buttons until two players ready
 // disableRPS();
 
-//  .info/connected (t/f)
+//  .info/connected (t/f). This function listens for connection/disconnection and assigns unique key in chronological order
 connectedRef.on("value", function (snap) {
     if (snap.val()) {
         var con = connectionsRef.push(true);      
-        key = con.getKey(); 
+        player.key = con.getKey(); 
+        console.log("player key: " + player.key)
         con.onDisconnect().remove().then(function(){
-              //removes the game after all players disconnect
-              gameRef.child(key).onDisconnect().remove();
+              //removes the game entry and chat messages for the player when disconnected
+              database.ref("/game/" + player.key).onDisconnect().remove();
+              database.ref("/messages/").onDisconnect().remove();
         });
         
     }
@@ -46,24 +60,78 @@ connectedRef.on("value", function (snap) {
 });
 
 
-//  /connections
+//  /connections   This function listens for number of connections present and stores the uid
 connectionsRef.on("value", function (snap) {
     numChildren = snap.numChildren();
-    $(".chat").text("num players: " + numChildren);
-
+    console.log("num child connections: " + numChildren)
+   
     if (numChildren == 2){
         //this is where we will enable the rps buttons for play
         $(".game-output").html("Ready to play! Make your choice. <br/>")
     } else if (numChildren == 1){
+
         $(".game-output").html("Waiting for opponent... <br/>")
     }
 
-
+  
 });
 
 
+// listen for player name
+database.ref("/connections/"  + player.key).on("child_changed", function(snap){
+    player.name = snap.val().name; 
+});
 
- 
+
+// listens for children of game and when 2 present (both users have guessed) provides a list of key/guess pairs with which to score
+database.ref("/game/").on("value", function(snap){
+    if(snap.numChildren() == 2){
+        console.log("2 guesses in db - ready to score")
+        var list = [];
+        snap.forEach(function(child){           
+                list.push({
+                    key: child.key,
+                    guess: child.val().guess
+                });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+        });
+        scoring(list)
+    }
+})
+
+function scoring(list){
+    //first figure out which player/guess is which and assign the opponent's key and guess
+    if (list[0].key === player.key){
+        opponent.guess = list[1].guess;
+        opponent.key = list[1].key;
+    } else {
+        opponent.key = list[0].key
+        opponent.guess = list[0].guess;
+    }
+
+    //then figure out who won/lost/tied. Output the results and increment the scores
+    if ((player.guess === "rock" && opponent.guess === "scissors") ||
+        (player.guess === "scissors" && opponent.guess === "paper") ||
+        (player.guess === "paper" && opponent.guess === "rock")) {
+        player.wins++;
+        $(".game-output").append("Opponent chose " + opponent.guess + "<br/>")
+        $(".game-output").append("You won! <br/>")
+        $("#wins").text("Wins: " + player.wins)
+    } else if (player.guess === opponent.guess) {
+        player.ties++;
+        $(".game-output").append("Opponent chose " + opponent.guess + "<br/>")
+        $(".game-output").append("You tied! <br/>")
+        $("#ties").text("Ties " + player.ties)
+    } else {
+        player.losses++;
+        $(".game-output").append("Opponent chose " + opponent.guess + "<br/>")
+        $(".game-output").append("You lost! <br/>")
+        $("#losses").text("Losses: " + player.losses)
+    }
+    //clear the player's game guesses
+    database.ref("/game/" + player.key).remove();
+}
+
+// gets user name and stores in database/outputs to page
 $("#save-btn").on("click", function (event) {
     event.preventDefault();
     
@@ -72,19 +140,16 @@ $("#save-btn").on("click", function (event) {
         player.name = name;  
         if (numChildren < 3){
              $(".player").append(": " + player.name)
-             gameRef.child(key).set({
-                
+             connectionsRef.child(player.key).set({                
                 name: player.name
             })
             $("#save-btn").prop("disabled", true);
         } else {
             $(".game-output").text("Two players are currently playing. Try again later.");
-        }
-        
+        }        
     } else {
         $(".game-output").text("Enter a name of at least 1 character, please.")
     }
-
     $("form").trigger("reset");
 
 });
@@ -101,57 +166,55 @@ function enableRPS(){
     $("#scissors-btn").prop("disabled", false);
 }
 
-
-
-
+                                                                                                                                                                                                                                                                                 
 $("#rock-btn").on("click", function () {
-    $(".game-output").append("You chose rock")
-    guess = "r";
-    gameRef.child(key).update({
-        
-        guess: guess
-       
-    });
-  
+    $(".game-output").append("You chose rock. <br/>")
+    player.guess = "rock";
+    database.ref("/game/" + player.key).set({        
+        guess: player.guess
+    }); 
 //    disableRPS()
 });
 
 $("#paper-btn").on("click", function () {
-    guess = "p"
-    $(".game-output").append("You chose paper <br/>");
-    gameRef.child(key).update({
-        
-        guess: guess
-       
+    player.guess = "paper"
+    $(".game-output").append("You chose paper. <br/>");
+    database.ref("/game/" + player.key).set({       
+        guess: player.guess
     });
 //    disableRPS()
 });
 
 $("#scissors-btn").on("click", function () {
-    guess = "c";
-    $(".game-output").append("You chose scissors <br/>")
-    gameRef.child(key).update({
-        
-        guess: guess
-       
+    player.guess = "scissors";
+    $(".game-output").append("You chose scissors. <br/>")
+    database.ref("/game/"+ player.key).set({
+        guess: player.guess
     });
 //    disableRPS()
 });
 
-//tie this logic to the rps buttons, score the game and reenable buttons
-if ((guess === "r") || (guess === "p") || (guess === "s")) {
 
-    if ((guess === "r" && computerGuess === "s") ||
-        (guess === "s" && computerGuess === "p") ||
-        (guess === "p" && computerGuess === "r")) {
-        wins++;
-    } else if (guess === computerGuess) {
-        ties++;
-    } else {
-        losses++;
-    }
+//--------MESSAGING--------------------------------
 
-}
+var messageRef = firebase.database().ref("/messages/");
+
+$("#msg-btn").on("click", function(event){
+
+    event.preventDefault();
+    var msg = $("#text-area").val().trim();
+    messageRef.push((player.name + ": " + msg))
+    $("form").trigger("reset");
+
+
+
+
+});
+
+
+messageRef.on("child_added", function(snap){
+    $(".chat").append(snap.val() + "<br/>")
+});
 
 
 
